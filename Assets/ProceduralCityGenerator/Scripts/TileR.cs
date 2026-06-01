a64cabc2c48685dc8915251de02e1a2da1749a92
Purna <purna@users.noreply.github.com>
2026-06-01 14:18:20 +0100

    v1

---
+++ Assets/ProceduralCityGenerator/Scripts/TileR.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProceduralCity
+{
+    public class TileR : MonoBehaviour
+    {
+        public TileR[] up;
+        public TileR[] right;
+        public TileR[] down;
+        public TileR[] left;
+
+        public bool isBuilding;
+
+    }
+}
